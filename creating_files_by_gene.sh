66989c237e7a49c13b13f7bab53722caf9565ec1
Daniel Witoslawski <122308546+DWitoslawski@users.noreply.github.com>
2024-01-16 14:56:50 -0800

    Update creating_files_by_gene.sh

--- creating_files_by_gene.sh
+++ creating_files_by_gene.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-all_genes='/home/dwito/merging_rmats_dexseq/genome_wide_analysis/all_genes.txt'
+all_genes='example/all_genes.txt'
 
 cat $all_genes | while read line; do
 	#echo -e "\n\nGene ID: $line\n"
@@ -8,32 +8,32 @@ cat $all_genes | while read line; do
 	mkdir gene_files/$line
 
 	#echo -e "A3SS events:" 
-	grep -w GeneID /home/dwito/RStudioFiles/fromGTF.A3SS.txt > gene_files/$line/fromGTF.A3SS.txt
-	grep -w $line /home/dwito/RStudioFiles/fromGTF.A3SS.txt >> gene_files/$line/fromGTF.A3SS.txt
+	grep -w GeneID fromGTF.A3SS.txt > example/$line/fromGTF.A3SS.txt
+	grep -w $line fromGTF.A3SS.txt >> example/$line/fromGTF.A3SS.txt
 	
 	#echo -e "\nA5SS events:" 
-	grep -w GeneID /home/dwito/RStudioFiles/fromGTF.A5SS.txt > gene_files/$line/fromGTF.A5SS.txt
-	grep -w $line /home/dwito/RStudioFiles/fromGTF.A5SS.txt >> gene_files/$line/fromGTF.A5SS.txt
+	grep -w GeneID fromGTF.A5SS.txt > example/$line/fromGTF.A5SS.txt
+	grep -w $line fromGTF.A5SS.txt >> example/$line/fromGTF.A5SS.txt
 	
 	#echo -e "\nSE events:"
-	grep -w GeneID /home/dwito/RStudioFiles/fromGTF.SE.txt > gene_files/$line/fromGTF.SE.txt	
-	grep -w $line /home/dwito/RStudioFiles/fromGTF.SE.txt >> gene_files/$line/fromGTF.SE.txt
+	grep -w GeneID fromGTF.SE.txt > example/$line/fromGTF.SE.txt	
+	grep -w $line fromGTF.SE.txt >> example/$line/fromGTF.SE.txt
 
 	#echo -e "\nRI events:"
-	grep -w GeneID /home/dwito/RStudioFiles/fromGTF.RI.txt > gene_files/$line/fromGTF.RI.txt
-        grep -w $line /home/dwito/RStudioFiles/fromGTF.RI.txt >> gene_files/$line/fromGTF.RI.txt
+	grep -w GeneID fromGTF.RI.txt > example/$line/fromGTF.RI.txt
+        grep -w $line fromGTF.RI.txt >> example/$line/fromGTF.RI.txt
 
 done
 
 
-find ./gene_files/ -type f -exec awk -v x=2 'NR==x{exit 1}' {} \; -exec  rm -f {} \;
-find ./gene_files/ -type d -empty -delete
+find ./example/ -type f -exec awk -v x=2 'NR==x{exit 1}' {} \; -exec  rm -f {} \;
+find ./example/ -type d -empty -delete
 
 
 
-ls gene_files > all_genes_updated.txt
+ls example > all_genes_updated.txt
 
-all_genes_updated=/home/dwito/merging_rmats_dexseq/genome_wide_analysis/all_genes_updated.txt''
+all_genes_updated='all_genes_updated.txt'
 
 cat $all_genes_updated | while read line; do
 	
