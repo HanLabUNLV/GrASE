b9f89b1dd94ea5866b0b533f9a2b03a20d733c30
Jelard Aquino <47357412+jelardaquino@users.noreply.github.com>
2024-03-08 14:14:20 -0800

    Update fig6_venn_diagram_exonicparts.R

--- scripts_figures/fig6_venn_diagram_exonicparts.R
+++ scripts_figures/fig6_venn_diagram_exonicparts.R
@@ -1,11 +1,11 @@
 library(eulerr)
 #DEXSeq exonic unit
 fit <- euler(c("rMATS detected" = 138997-95503-166,
-               "DEXSeq sig" = 3647-817-166-289,
-               "DEXSeq sig&rMATS detected" = 166,
+               "DEXSeq sig" = 3647-917,
+               "DEXSeq sig&rMATS detected" = 100,
                "rMATS detected&rMATS tested" = 138997-95503-7432,
                "rMATS detected&rMATS tested&rMATS sig" = 7432-289,
-               "rMATS detected&rMATS tested&DEXSeq sig" = 917-289,
+               "rMATS detected&rMATS tested&DEXSeq sig" = 917-100-289,
                "rMATS detected&rMATS tested&rMATS sig&DEXSeq sig" = 289))
 
 plot(fit,
