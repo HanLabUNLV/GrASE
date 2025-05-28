7165aded30019e169c1ba75abf00b3cd5e02910c
Mira Han <mira.han@unlv.edu>
2025-05-27 18:50:54 -0700

    bugfix: properly remove duplicates and update nodes and edges when two nodes are equivalent (same coordinate) in the splicing graph

--- Rpkg/R/graph_utils.R
+++ Rpkg/R/graph_utils.R
@@ -55,7 +55,17 @@ SG2igraph <- function(geneID,  gene_sg, gene_graph) {
   }, USE.NAMES = FALSE)
   #write.table(g1.df, paste0(geneID, ".edgetable.txt"),  row.names=FALSE, sep="\t", quote=FALSE, col.names = TRUE)
 
-  node_coord = node_coord[!duplicated(node_coord$coord),] 
+  duplicated_coords <- node_coord %>%
+    group_by(coord) %>%
+    filter(n() > 1) %>%
+    ungroup()
+  dup_keep = as.data.frame(duplicated_coords)[1,'sgid']
+  dup_replace = as.data.frame(duplicated_coords)[2,'sgid']
+  
+  g1.df$from[g1.df$from == dup_replace] = dup_keep
+  g1.df$to[g1.df$to == dup_replace] = dup_keep
+
+  node_coord = node_coord[node_coord$sgid != dup_replace,]
   nodes.df = data.frame( ID=node_coord$sgid)
   #write.table(nodes.df, paste0(geneID, ".vertices.txt"),  row.names=FALSE, sep="\t", quote=FALSE, col.names = TRUE)
 
